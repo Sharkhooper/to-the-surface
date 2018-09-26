c385e87aff707020403f56a43ce16e619fa7eca2
Jan <meissnej@hochschule-trier.de>
2018-09-26 15:25:17 +0200

    LevelSelect now dynamically (un-)locks levels

--- Assets/_Game/Scripts/LevelSelectController.cs
+++ Assets/_Game/Scripts/LevelSelectController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.UI;
 
 public class LevelSelectController : MonoBehaviour
 {
@@ -12,18 +13,53 @@ public class LevelSelectController : MonoBehaviour
 	void Start ()
 	{
 		gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+		ActivateAllPages();
 		UpdateLevelSelect();
+		DisablePagesAfterFirst();
 	}
 
-	void UpdateLevelSelect()
+	// Locks all currently not unlocked levels
+	public void UpdateLevelSelect()
 	{
-		Debug.Log("Updatet LevelSelect");
 		int highestLevel = gm.HighestLevel;
+		
 		//Static implementation for last level
-		for (int i = highestLevel+1; i < 18; i++)
+		for (int i = highestLevel+1; i <= 18; i++)
 		{
 			String buttonName = "ButtonLevel" + i;
-			GameObject button = GameObject.Find(buttonName);
+			if (GameObject.Find(buttonName) != null)
+			{
+				GameObject button = GameObject.Find(buttonName);
+				Image image = button.transform.Find("Image").GetComponent<Image>();
+				image.sprite = Resources.Load("lockedTEMP", typeof(Sprite)) as Sprite;
+				button.GetComponent<Button>().interactable = false;
+			}
+			else
+			{
+				Debug.Log("Button not found");
+			}
+		}
+	}
+
+	//Disables all pages except the first one
+	private void DisablePagesAfterFirst()
+	{
+		bool firstPage = true;
+		foreach(GameObject page in GameObject.FindGameObjectsWithTag("LevelSelectPage"))
+		{
+			if(firstPage)
+				firstPage = false;
+			else
+				page.SetActive(false);
+		}
+	}
+
+	//Ensures all pages are active before loading "locked" images
+	private void ActivateAllPages()
+	{
+		foreach(GameObject page in GameObject.FindGameObjectsWithTag("LevelSelectPage"))
+		{
+			page.SetActive(true);
 		}
 	}
 }
