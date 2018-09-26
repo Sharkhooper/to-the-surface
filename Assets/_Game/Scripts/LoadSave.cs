2d54394f053518b96583c3364fe437373b728a44
Marcel <kuehnma@hochschule-trier.de>
2018-09-26 15:36:52 +0200

    Speicher System implementiert

---
+++ Assets/_Game/Scripts/LoadSave.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary; 
+using System.IO;
+using UnityEngine;
+
+
+[System.Serializable]
+public class LoadSave
+{
+
+
+
+	private int currentLevelSafe,highestLevelSafe;
+	private static string SaveFile => $"{Application.persistentDataPath}/save.json";
+	
+	
+	
+	
+	
+	private struct SaveData
+	{
+		public int currentLevelSafe;
+		public int highestLevelSafe;
+	}
+
+	
+	
+	
+	
+	public void Safe(int currentLevel, int highestLevel)
+	{
+		
+		
+		SaveData data = new SaveData {
+			currentLevelSafe = currentLevel,
+				highestLevelSafe = highestLevel
+		};
+
+		
+		StreamWriter sw = new StreamWriter(SaveFile);
+		sw.WriteLine(JsonUtility.ToJson(data));
+		sw.Close();
+		
+		
+		
+	}
+
+
+	public void Load(out int currentLevel, out int highestLevel)
+	{
+		
+		StreamReader sr = new StreamReader(SaveFile);
+		SaveData save = JsonUtility.FromJson<SaveData>(sr.ReadToEnd());
+		sr.Close();
+		currentLevel = save.currentLevelSafe;
+		highestLevel = save.highestLevelSafe;
+	
+		
+		
+	}
+
+
+	public bool DoesSaveFileExist()
+	{
+
+		return File.Exists(SaveFile);
+	}
+
+
+
+
+
+
+
+
+}
