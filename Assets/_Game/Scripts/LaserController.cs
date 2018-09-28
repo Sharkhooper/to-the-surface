b9a5e40b8b539ea1c7ce8c4ec5d9965f25dc99af
Markus Schlösser <schloem@fh-trier.de>
2018-09-28 12:26:44 +0200

    Added laser controller

---
+++ Assets/_Game/Scripts/LaserController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour {
+
+	[SerializeField] private Sprite active;
+	[SerializeField] private Sprite inactive;
+
+	[SerializeField] private List<SpriteRenderer> endpoints;
+	[SerializeField] private List<SpriteRenderer> laserRenderer;
+	[SerializeField] private List<Collider2D> lasercollider;
+
+	public void SetState(bool value) {
+		foreach (SpriteRenderer r in endpoints) {
+			r.sprite = value ? active : inactive;
+		}
+
+		foreach (SpriteRenderer r in laserRenderer) {
+			r.enabled = value;
+		}
+
+		foreach (Collider2D c in lasercollider) {
+			c.enabled = value;
+		}
+	}
+}
