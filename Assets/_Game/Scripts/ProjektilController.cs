9f58eadbc758602e5948a7787ff2321e7db1e4f7
Jan <meissnej@hochschule-trier.de>
2018-09-28 10:28:04 +0200

    Optimized ProjektilController

--- Assets/_Game/Scripts/ProjektilController.cs
+++ Assets/_Game/Scripts/ProjektilController.cs
@@ -1,16 +1,9 @@
 ﻿using UnityEngine;
 
 public class ProjektilController : MonoBehaviour {
+	
 	private void OnTriggerEnter2D(Collider2D other) {
-		Debug.Log(other.name);
-		if (other.gameObject.name != "Cannon" || other.gameObject.name != "CannonBarrel") { }
-		else {
-			Destroy(gameObject);
-			PlayerActor player = other.GetComponent<PlayerActor>();
-			if (player != null) {
-				player.Die();
-			}
-		}
+		Destroy(gameObject);
 	}
 
 	void OnBecameInvisible() {
